3654a337c621c117d99fabe968179ba6b9c89be2
Fernando Nogueira <fernando.cossta@gmail.com>
2025-03-07 16:46:11 -0300

    Add cross-origin package to hot reload

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,14 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
+
+const URL = 'http://localhost:1337'
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL(URL)
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
