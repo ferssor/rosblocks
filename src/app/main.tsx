79497cb1355d0caa761039399deea4a8e31e29f7
Fernando Nogueira <fernando.cossta@gmail.com>
2025-03-10 15:06:30 -0300

    Add logo component

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { App, ConfigProvider, Layout } from "antd";
+import Logo from "./components/Logo";
 
+const { Header, Sider, Content } = Layout;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <p>Welcome page</p>
+    <ConfigProvider>
+      <App>
+        <Header>
+          <Logo />
+        </Header>
+        <Layout>
+          <Sider />
+          <Content />
+        </Layout>
+      </App>
+    </ConfigProvider>
   </StrictMode>
 );
