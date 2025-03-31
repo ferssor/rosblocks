174978676d2cb308888bc89facc645ee6e2e7fe6
Fernando Nogueira <fernando.cossta@gmail.com>
2025-03-31 11:01:49 -0300

    WIP: Add editor header buttons

--- src/app/components/Editor/components/SelectPackage/Components/NodeManager/components/NodeEditor/index.tsx
+++ src/app/components/Editor/components/SelectPackage/Components/NodeManager/components/NodeEditor/index.tsx
@@ -1,7 +1,12 @@
-import { Layout } from "antd";
+import { Button, Layout, Tooltip } from "antd";
 import "./styles.css";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+import {
+  ApartmentOutlined,
+  CodeOutlined,
+  FileTextOutlined,
+} from "@ant-design/icons";
 
 interface Props {
   selectedNode: ROSNode;
@@ -14,11 +19,24 @@ function NodeEditor(props: Props) {
   return (
     <>
       <Layout className="layout-container">
-        <Header className="header-container">{selectedNode.name}</Header>
+        <Header className="header-container">
+          <div className="editor-actions">
+            <Tooltip title="Mostrar editor de blocos">
+              <ApartmentOutlined />
+            </Tooltip>
+            <Tooltip title="Mostrar terminal">
+              <CodeOutlined />
+            </Tooltip>
+            <Tooltip>
+              <FileTextOutlined title="Mostrar editor de texto" />
+            </Tooltip>
+          </div>
+          <Button type="primary">Salvar</Button>
+        </Header>
         <Layout>
           <Content className="content-container">Blockly</Content>
           <Sider className="sider-container" width="50%">
-            Monaco Editor
+            {selectedNode.content}
           </Sider>
         </Layout>
         <Footer className="footer-container">Terminal</Footer>
